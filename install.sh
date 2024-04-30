b20b9ac886a21252ae0d6cd734fed28d61e926f5
Yahya Jabary <jabaryyahya@gmail.com>
2024-05-01 01:23:47 +0200

    up

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
+if ! command -v java &> /dev/null; then echo "java missing"; exit 1; fi
 if ! command -v python3 &> /dev/null; then echo "python3 missing"; exit 1; fi
 if ! command -v pip &> /dev/null; then echo "pip missing"; exit 1; fi
 
-python3 -m pip install --upgrade pip > /dev/null
+# pyarrow only works up to python 3.11 (as of 2024-05-01)
+# see: https://stackoverflow.com/a/77318636/13045051
 
-pip install black > /dev/null
-pip install pipreqs > /dev/null
+brew install python@3.11
+
+# ---
+
+python3.11 -m pip install --upgrade pip > /dev/null
+
+python3.11 -m pip install black > /dev/null
+python3.11 -m pip install pipreqs > /dev/null
 
 rm -rf requirements.txt > /dev/null
 pipreqs . > /dev/null
-pip install -r requirements.txt > /dev/null
+python3.11 -m pip install -r requirements.txt > /dev/null
+
+# ---
+
+python3.11 -m pip install pyspark 'pyspark[sql]' 'pyspark[ml]' 'pyspark[streaming]'
---
+++ src/spark.py
@@ -0,0 +1,4 @@
+import spark
+from pyspark.sql import SparkSession
+
+# https://spark.apache.org/docs/3.3.1/api/python/index.html
