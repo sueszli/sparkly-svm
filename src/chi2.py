932ff9c4c23d27191eb16ea15263d07302eaa3c7
Yahya Jabary <jabaryyahya@gmail.com>
2024-05-01 02:19:31 +0200

    up

--- src/chi2.py
+++ src/chi2.py
@@ -1,7 +1,10 @@
 import pyspark
 from pyspark.sql import SparkSession
+
 import pathlib
 import re
+from collections import defaultdict
+
 
 sc = pyspark.SparkContext("local", "app")
 sc.setLogLevel("ERROR")
@@ -18,4 +21,9 @@ stopwords = pathlib.Path(__file__).parent.parent / "data" / "stopwords.txt"
 stopwords = sc.textFile(str(stopwords)).collect()
 terms_cat = text_cat.map(lambda x: ([t for t in re.split(regex, x[0].lower()) if t and t not in stopwords], x[1]))
 
+N: int = terms_cat.count()
+cat_count: defaultdict = terms_cat.map(lambda x: (x[1], 1)).reduceByKey(lambda x, y: x + y).collectAsMap()  # type: ignore
+
+print(N)
+print(cat_count)
 print(terms_cat.take(1))
