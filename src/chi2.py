882929ef6325560cc87ffcd1a8dff7a691646b73
Yahya Jabary <jabaryyahya@gmail.com>
2024-05-01 15:23:54 +0200

    up

--- src/chi2.py
+++ src/chi2.py
@@ -59,15 +59,20 @@ print(f"term_cat_count in {time.perf_counter() - stime:.4f}s -> {term_cat_count.
 # probably faster to compute each of terms above in one go as it takes a while to start up the spark context
 
 
-# def get_cat_term_chi(term: str, cat: str) -> tuple[str, str, float]:
-#     tc11 = term_cat_count.filter(lambda x: x[0] == term and x[1] == cat).map(lambda x: x[2]).first()
-#     tc10 = term_count.filter(lambda x: x[0] == term).map(lambda x: x[1]).first() - tc11
-#     tc01 = cat_count.filter(lambda x: x[0] == cat).map(lambda x: x[1]).first() - tc11
-#     tc00 = N - tc11 - tc10 - tc01
-#     print(tc11, tc10, tc01, tc00)
-#     return term, cat, (N * (tc11 * tc00 - tc10 * tc01) ** 2) / ((tc11 + tc10) * (tc11 + tc01) * (tc10 + tc00) * (tc01 + tc00))
+def get_cat_term_chi(term: str, cat: str) -> tuple[str, str, float]:
+    tc11 = get_term_cat_count(term, cat)
+    tc10 = get_term_count(term) - tc11
+    tc01 = get_cat_count(cat) - tc11
+    tc00 = N - tc11 - tc10 - tc01
+    num = N * (tc11 * tc00 - tc10 * tc01) ** 2
+    den = (tc11 + tc10) * (tc11 + tc01) * (tc10 + tc00) * (tc01 + tc00)
+    assert den != 0
+    return term, cat, num / den
 
 
-# cat_term_chi = term_cat_count.map(lambda x: get_cat_term_chi(x[0][0], x[0][1]))
+stime = time.perf_counter()
+cat_term_chi = term_cat_count.map(lambda x: get_cat_term_chi(x[0][0], x[0][1]))
+print(f"cat_term_chi in {time.perf_counter() - stime:.4f}s -> {cat_term_chi.take(1)}")
+
 
 sc.stop()
