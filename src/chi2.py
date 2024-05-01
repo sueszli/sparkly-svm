caaacb0de02d668761351fbbddc26bbefd9815bc
Yahya Jabary <jabaryyahya@gmail.com>
2024-05-01 16:23:20 +0200

    up

--- src/chi2.py
+++ src/chi2.py
@@ -26,28 +26,24 @@ terms_cat: RDD = sc.textFile(str(datapath)) \
                 .map(json.loads) \
                 .map(lambda jsn: (jsn["reviewText"], jsn["category"])) \
                 .map(lambda tc: (get_terms(tc[0]), tc[1]))  # type: ignore
+terms_cat.persist()
 # fmt: on
-print(f"terms_cat -> {terms_cat.take(1)}")
 
 
-# total num
-N: int = sc.textFile(str(datapath)).count()
-print(f"N -> {N}")
-
-
+"""
+precompute
+"""
 # [(cat, count), ...]
 cat_count: RDD = terms_cat.map(lambda x: (x[1], 1)).reduceByKey(lambda a, b: a + b)  # type: ignore
 cat_count_broadcast = sc.broadcast(dict(cat_count.collect()))
 get_cat_count = lambda cat: cat_count_broadcast.value[cat]
 print(f"cat_count -> {cat_count.take(1)}")
 
-
 # [(term, count), ...]
 term_count: RDD = terms_cat.flatMap(lambda x: [(t, 1) for t in x[0]]).reduceByKey(lambda a, b: a + b)  # type: ignore
 term_count_broadcast = sc.broadcast(dict(term_count.collect()))
 get_term_count = lambda term: term_count_broadcast.value[term]
 print(f"term_count -> {term_count.take(1)}")
-
 
 # [((term, cat), count), ...]
 term_cat_count: RDD = terms_cat.flatMap(lambda x: [((t, x[1]), 1) for t in x[0]]).reduceByKey(lambda a, b: a + b)  # type: ignore
@@ -56,8 +52,12 @@ get_term_cat_count = lambda term, cat: term_cat_count_broadcast.value[(term, cat
 print(f"term_cat_count -> {term_cat_count.take(1)}")
 
 
+"""
+chi2 computation
+"""
 # [(cat, [(term, chi2), ...]), ...]
 # fmt: off
+N: int = sc.textFile(str(datapath)).count()
 get_chi2 = lambda n11, n10, n01, n00: N * (n11 * n00 - n10 * n01) ** 2 / ((n11 + n10) * (n01 + n00) * (n11 + n01) * (n10 + n00))
 cat_term_chi2s: RDD = term_cat_count \
     .map(lambda tc_c: (
@@ -76,7 +76,6 @@ cat_term_chi2s: RDD = term_cat_count \
     .sortByKey() \
     .mapValues(lambda t_chi2s: sorted(t_chi2s, key=lambda x: x[1], reverse=True)[:75])
 # fmt: on
-print(f"cat_term_chi2s -> {cat_term_chi2s.take(1)}")
 
 # print all cat_term_chi2s
 for cat, term_chi2s in cat_term_chi2s.collect():
