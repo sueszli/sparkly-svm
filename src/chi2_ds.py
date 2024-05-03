1264eed1665ec91ab78d23b2031a3125d4a94a07
Yahya Jabary <jabaryyahya@gmail.com>
2024-05-03 02:28:28 +0200

    up

--- src/chi2_ds.py
+++ src/chi2_ds.py
@@ -1,7 +1,9 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import Tokenizer
-from pyspark.sql.functions import col
+from pyspark.ml.feature import RegexTokenizer
+from pyspark.ml.feature import StopWordsRemover
+
 
 import pathlib
 
@@ -14,10 +16,21 @@ conf = SparkConf().setAppName("chi2").setMaster("local[*]")
 sc = SparkContext(conf=conf)
 spark = SparkSession(sc)
 
-df = spark.read.json(str(DATA_PATH))
-df = df.select("reviewText", "category")
 
-# tokenize
-tokenizer = Tokenizer(inputCol="reviewText", outputCol="words")
-df = tokenizer.transform(df)
+"""
+tokenization, case folding, stopword removal
+"""
+# fmt: off
+regex = r'[ \t\d()\[\]{}.!?,;:+=\-_"\'~#@&*%€$§\/]+'
+df = RegexTokenizer(inputCol="reviewText", outputCol="terms", pattern=regex) \
+    .transform(spark.read.json(str(DATA_PATH))) \
+    .select("terms", "category")
+
+stopwords = sc.textFile(str(STOPWORD_PATH)).collect()
+df = StopWordsRemover(inputCol="terms", outputCol="filtered", stopWords=stopwords) \
+    .transform(df) \
+    .select("filtered", "category") \
+    .withColumnRenamed("filtered", "terms")
+# fmt: on
+
 print(df.show())
